8430a205ccdef505a26cfd8c900ceb9f595c7d3b
yamami mohamed monsif <mohammedyamami05@gmail.com>
2025-07-19 16:47:38 +0100

    updated the FAQItem - imported motion from motionProvider and wrapped everuthing in MotionProvider

--- components/UI/FAQItem.tsx
+++ components/UI/FAQItem.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { ChevronUp } from "lucide-react";
-import { motion, AnimatePresence } from "framer-motion";
+import {
+  motion,
+  AnimatePresence,
+  MotionProvider,
+} from "@/utils/MotionProvider";
 
 interface FAQItemProps {
   question: string;
@@ -12,34 +16,38 @@ interface FAQItemProps {
 
 const FAQItem = ({ question, answer, isOpen, onToggle }: FAQItemProps) => {
   return (
-    <div className="border-b border-gray-200 last:border-none">
-      <button
-        onClick={onToggle}
-        className="w-full flex items-center justify-between py-4 text-left hover:cursor-pointer"
-      >
-        <h3 className="text-gray-900 font-medium pr-4">{question}</h3>
-        <motion.div
-          animate={{ rotate: isOpen ? 180 : 0 }}
-          transition={{ duration: 0.2, ease: "easeInOut" }}
-          style={{ display: "flex", alignItems: "center" }}
+    <MotionProvider>
+      <div className="border-b border-gray-700 last:border-none">
+        <button
+          onClick={onToggle}
+          className="w-full flex items-center justify-between py-4 text-left"
+          aria-expanded={isOpen}
+          aria-controls="faq-answer"
         >
-          <ChevronUp className="w-5 h-5 text-gray-900" />
-        </motion.div>
-      </button>
-      <AnimatePresence mode="sync">
-        {isOpen && (
+          <h3 className="text-gray-900 font-medium pr-4">{question}</h3>
           <motion.div
-            initial={{ height: 0, opacity: 0 }}
-            animate={{ height: "auto", opacity: 1 }}
-            exit={{ height: 0, opacity: 0 }}
+            animate={{ rotate: isOpen ? 180 : 0 }}
             transition={{ duration: 0.2, ease: "easeInOut" }}
-            className="overflow-hidden"
+            style={{ display: "flex", alignItems: "center" }}
           >
-            <p className="pb-4 text-gray-800 text-sm">{answer}</p>
+            <ChevronUp className="w-5 h-5 text-gray-900" />
           </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
+        </button>{" "}
+        <AnimatePresence mode="sync">
+          {isOpen && (
+            <motion.div
+              initial={{ height: 0, opacity: 0 }}
+              animate={{ height: "auto", opacity: 1 }}
+              exit={{ height: 0, opacity: 0 }}
+              transition={{ duration: 0.2, ease: "easeInOut" }}
+              className="overflow-hidden"
+            >
+              <p className="pb-4 text-gray-800 text-sm">{answer}</p>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </MotionProvider>
   );
 };
 
