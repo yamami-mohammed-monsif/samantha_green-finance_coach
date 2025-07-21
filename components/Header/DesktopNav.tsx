d8274cf1472466ccdc66c2719787ee9724f027b8
yamami mohamed monsif <mohammedyamami05@gmail.com>
2025-07-21 15:01:03 +0100

    added hover effect to navlinks

--- components/Header/DesktopNav.tsx
+++ components/Header/DesktopNav.tsx
@@ -3,7 +3,7 @@ import { NavLinks } from "@/constants";
 
 export default function DesktopNav() {
   const baseClasses =
-    "text-base text-white transition-all duration-200 hover:scale-110";
+    "text-base text-white transition-all duration-200 hover:scale-110 hover:text-gray-900";
 
   return (
     <nav className="hidden lg:flex items-center gap-10">
